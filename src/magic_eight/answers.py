d97093bdfed38c6a6a204d383dab76adfe04637b
Natalie Trump <nht251@nyu.edu>
2024-10-22 15:14:06 +0300

    add core functions for package

---
+++ src/magic_eight/answers.py
@@ -0,0 +1,12 @@
+answers ={
+    "en": [
+        "Don't count on it",
+        "Yes",
+        "No",
+        "Maybe",
+        "Ask again later",
+        "I doubt it",
+        "Certainly",
+        "I don't know"
+    ]
+}
