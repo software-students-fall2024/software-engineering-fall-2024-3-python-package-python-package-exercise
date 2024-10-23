0b130a96a8c489e868e3250a04bfe7ef0aaeac1f
JosephChege4 <chegejrmungai@gmail.com>
2024-10-23 11:12:26 -0400

    pyexcuses first function

--- pyexcuses/pyexcuses.py
+++ pyexcuses/pyexcuses.py
@@ -5,46 +5,27 @@ from .excuses_en import excuses_en
 from .excuses_es import excuses_es
 from .exc import LanguageNotFoundError, CategoryNotFoundError
 
-
 all_excuses = {
     "en": excuses_en,
     "es": excuses_es,
 }
 
+SPOKEN_LANGUAGES = Literal["en", "es"]
+PROGRAMMING_LANGUAGES = Literal["neutral", "javascript", "python", "matlab", "all"]
 
-LANGUAGES = Literal["en", "es"]
-CATEGORIES = Literal["neutral", "javascript", "python", "matlab", "all"]
+SPOKENLANGUAGE_VALUES = set(get_args(SPOKEN_LANGUAGES))
+PROGRAMMINGLANGUAGE_VALUES = set(get_args(PROGRAMMING_LANGUAGES))
 
-LANGUAGE_VALUES = set(get_args(LANGUAGES))
-CATEGORY_VALUES = set(get_args(CATEGORIES))
-
-
-def get_excuses(language: LANGUAGES = "en", category: CATEGORIES = "neutral") -> list[str]:
+def generate_excuse(spoken_language: SPOKEN_LANGUAGES = "en", programming_language: PROGRAMMING_LANGUAGES = "neutral") -> str:
     """
-    Get a list of excuses from the given language and category
+    Get a single excuse from the given spoken and programming languages
     """
     try:
-        excuses = all_excuses[language]
+        excuses = all_excuses[spoken_language]
     except KeyError:
-        raise LanguageNotFoundError(f"No such language: {language}")
-
+        raise LanguageNotFoundError("That spoken language isn't in our system ):")    
+    
     try:
-        return excuses[category]
+        return random.choice(excuses[programming_language])
     except KeyError:
-        raise CategoryNotFoundError("No such category %s in language %s" % (category, language))
-
-
-def get_excuse(language: LANGUAGES = "en", category: CATEGORIES = "neutral") -> str:
-    """
-    Get a single excuse from the given language and category
-    """
-    excuses = get_excuses(language, category)
-    return random.choice(excuses)
-
-
-def forever(language: LANGUAGES = "en", category: CATEGORIES = "neutral") -> Generator[str, Any, Any]:
-    """
-    Generate excuses forever
-    """
-    while True:
-        yield get_excuse(language, category)
+        raise CategoryNotFoundError("That programming language isn't in our system ):")
