1eca9c50f1074e7e7ae0af47723a9089f285d1b1
JosephChege4 <chegejrmungai@gmail.com>
2024-10-23 11:19:54 -0400

    adding solution files

---
+++ pyexcuses/solutions_es.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# SPANISH
+
+neutral = [
+    "¿Has probado a añadir más comentarios a tu código?",
+    "¿Has probado a encender y apagar tu ordenador de nuevo?",
+]
+
+javascript = [
+    "¿Has probado a añadir más puntos y comas? A JavaScript le encantan.",
+]
+
+python = [
+    
+]
+
+matlab = [ 
+    
+]
+
+jokes_en = {
+    "neutral": neutral,
+    "python": python,
+    "javascript": javascript,
+    "matlab": matlab,
+    "all": neutral + javascript + python + matlab,
+}
