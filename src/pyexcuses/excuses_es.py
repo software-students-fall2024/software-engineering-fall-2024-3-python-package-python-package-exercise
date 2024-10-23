161208854997a02da0292f99ee529bbdc238aedb
JosephChege4 <chegejrmungai@gmail.com>
2024-10-23 12:06:38 -0400

    spanish translation improvement

--- src/pyexcuses/excuses_es.py
+++ src/pyexcuses/excuses_es.py
@@ -10,11 +10,15 @@ javascript = [
 ]
 
 python = [
-
+    "Debe ser un problema de Python 2 vs. Python 3.",
+    "El entorno virtual debe estar funcionando mal.",
+    "Funcionó cuando lo ejecuté sin pytest.",
 ]
 
 matlab = [ 
-
+    "Es un error de redondeo; a MATLAB le encanta su precisión de punto flotante.",
+    "Podría ser una discrepancia en la dimensión de la matriz. MATLAB es muy exigente con eso.",
+    "Creo que el servidor de licencias de MATLAB debe estar inactivo nuevamente.",
 ]
 
 jokes_en = {
