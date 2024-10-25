16c560ee9397e200a6db1547e761e487cf9e6cf3
jnahan <jenna5376@gmail.com>
2024-10-25 19:21:43 -0400

    created helper function to get random message from list

---
+++ pyanimals/main.py
@@ -0,0 +1,13 @@
+import random
+
+# helper functions
+
+# print animal
+# def print_animal(animal):
+
+# print message
+# def print_message(message):
+
+# get random message from list
+def get_random_message(messages):
+    return random.choice(messages)
