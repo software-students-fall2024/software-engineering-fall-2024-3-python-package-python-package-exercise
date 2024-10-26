cd33dcdd432e1e403dbc422be0ce5b1203428940
haleyhobbs <hkh9725@nyu.edu>
2024-10-26 18:22:55 -0400

    created test file

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+from unittest.mock import patch
+import pytest
+# from pyanimals.main import PRIMARY FUNCTIONS
+from pyanimals.main import print_animal, get_random_message#, print_mesage
+
+# move test #1: 
+# def test_move_XX():
+
+# move test #2: animal moved
+# def test_move_completed():
+
+# move test #3: animal didn't move off screen
+# def test_move_withinBounds():
