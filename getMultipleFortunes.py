5b28244bf462aca4e94cf3cbfc14a12ee019c168
bryccce <qy765@nyu.edu>
2024-10-26 23:15:01 -0400

    create getMultipleFortunes function

--- getMultipleFortunes.py
+++ getMultipleFortunes.py
@@ -1,1 +1,25 @@
-print("Hello")
+import random
+
+def getMultipleFortunes(n):
+    file_path = 'resource/fortune.txt'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        fortunes = file.read().split('%')
+        fortunes = [fortune.strip() for fortune in fortunes if fortune.strip()]
+
+    result = []
+    for _ in range(n):
+        num = random.randint(1, 100)
+        fortune = random.choice(fortunes)
+        while fortune in result:
+            fortune = random.choice(fortunes)
+        result.append([fortune, num])       
+    
+    return result
+
+
+result = getMultipleFortunes(5)
+for fortune, num in result:
+    print(f"Fortune: {fortune}\nNumber: {num}\n")
+
+
+
