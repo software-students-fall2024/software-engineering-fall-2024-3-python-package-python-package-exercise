9c782a09b85571bb8e4f9170b3bf3826908a9bd7
mabowen1013 <mbw3047256@gmail.com>
2024-10-28 14:51:20 -0400

    add more test func, add more zodiac options

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,43 @@
 import pytest
+
 from funky_fortune.main import zodiac_fortune, lucky_number, fortune_cookie, magic_8ball
 
 def test_zodiac_fortune():
     assert zodiac_fortune("Aries") in ["You'll meet a talking sheep today", "Watch out for ram horns", "Your wool sweater will bring good luck"]
     assert zodiac_fortune("Taurus") in ["Today is perfect for beef noodles", "You'll find a gold coin", "Don't be as stubborn as a bull"]
 
+# test if there is no assigned zodiac, it will return default response
+def test_zodiac_fortune_special_sign():
+    assert zodiac_fortune("UnknownZodiac") == "Your zodiac sign is too special, I can't predict it"
+
+
+
+
+
+
 def test_lucky_number():
     
     assert 0 <= lucky_number("RandomName") < 100
+
+# make sure same name return same lucky number
+def test_lucky_number_consistency():
+    assert lucky_number("sameName") == lucky_number("sameName")
+
+# make sure special character also return valid lucky number
+def test_lucky_number_edge_cases():
+    assert lucky_number("") == 0
+    assert 0 <= lucky_number("123!@#") < 100
 
 def test_fortune_cookie():
     assert fortune_cookie() in ["You'll be very lucky today", "Slow and steady wins the race", "Today is perfect for trying new things", "Remember to call your family"]
     assert fortune_cookie(lucky=True).startswith("Extra lucky: ")
     for _ in range(100):
         assert fortune_cookie().startswith("Extra lucky: ") == False
+
+# make sure the return results are random
+def test_fortune_cookie_randomness():
+    results = {fortune_cookie() for _ in range(10)}
+    assert len(results)>1
 
 def test_magic_8ball():
     question = "Will I become rich today?"
