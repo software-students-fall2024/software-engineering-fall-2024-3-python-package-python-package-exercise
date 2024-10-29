f13ce59de0b01712d620abe7bafc0d39c48f8311
Applejam-ovo <ql2138@nyu.edu>
2024-10-29 04:28:48 -0400

    add setup trying to fix pull request check problem

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="virtualpet",
+    version="0.1.0",
+    description="A virtual pet simulator package.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",  
+    author="Applejam-ovo",
+    author_email="ql2138@nyu.edu",
+    url="https://github.com/software-students-fall2024/3-python-package-straight-a",
+    packages=find_packages(where="src"),  
+    package_dir={"": "src"},  
+    python_requires=">=3.7", 
+    install_requires=[],
+)
