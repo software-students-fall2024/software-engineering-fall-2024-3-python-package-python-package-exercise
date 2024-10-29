57455b2f1bd43081cdf6b528f1dc6eae982de903
Applejam-ovo <ql2138@nyu.edu>
2024-10-29 04:38:26 -0400

    Further changes to setup

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ setup(
     author="Applejam-ovo",
     author_email="ql2138@nyu.edu",
     url="https://github.com/software-students-fall2024/3-python-package-straight-a",
-    packages=find_packages(where="src"),  
-    package_dir={"": "src"},  
+    packages=find_packages(exclude=["tests"]),  
     python_requires=">=3.7", 
     install_requires=[],
 )
