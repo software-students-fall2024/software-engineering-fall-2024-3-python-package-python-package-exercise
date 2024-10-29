c891942cb25b04712c9079181bccafd958e40cee
saharbueno <saharbueno@gmail.com>
2024-10-29 19:28:09 -0400

    added first function to tamagotchi package

---
+++ src/tamagotchi/__main__.py
@@ -0,0 +1,12 @@
+from tamagotchi import tamagotchi as t
+
+def main():
+    t.getpet(1)
+    print('1----')
+    t.getpet(2)
+    print('2----')
+    t.getpet(3)
+    print('3----')
+
+if __name__ == "__main__":
+    main()
