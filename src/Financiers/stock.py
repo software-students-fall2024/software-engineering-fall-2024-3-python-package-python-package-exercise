d96860652251b2deef8cfe8cb8737e42ba491454
reyhanquayum <reyhan.quayum@gmail.com>
2024-10-29 20:34:21 -0400

    set up API

--- src/Financiers/stock.py
+++ src/Financiers/stock.py
@@ -1,1 +1,10 @@
 import pandas as pd
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+class Stock:
+    def __init__(self):
+        self.api_key = os.getenv("ALPHAVANTAGE_API_KEY")
+        
