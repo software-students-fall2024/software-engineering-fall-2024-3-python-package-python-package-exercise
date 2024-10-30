4dd2ade958cd874361bc6d319a50f961b89b69e2
annabellalee0113 <annabellalee0113@gmail.com>
2024-10-30 14:13:22 -0400

    update timer

--- timer.py
+++ timer.py
@@ -6,7 +6,7 @@ def timer(countdown_time):
         stop_time = random.randint(1, countdown_time - 1)
         for remaining in range(countdown_time, stop_time, -1):
             print(f"Time left: {remaining} seconds")
-            time.sleep(1)
+            time.sleep(random.randint(1, 5))
         print("Done!")
     else:
         for remaining in range(countdown_time, 0, -1):
