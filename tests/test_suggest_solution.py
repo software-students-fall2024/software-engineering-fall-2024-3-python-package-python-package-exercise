fbb686910f6f63abcfd02323c3d38288789146ef
JosephChege4 <chegejrmungai@gmail.com>
2024-10-30 15:11:50 -0400

    more english and spanish excuses and solutions

--- tests/test_suggest_solution.py
+++ tests/test_suggest_solution.py
@@ -4,53 +4,53 @@ from pyexcuses.exc import SpokenLanguageNotFoundError
 from pyexcuses.exc import ProgrammingLanguageNotFoundError
 
 
-def test_generate_excuse_default():
+def test_suggest_solution_default():
     """
-    Verify generate_excuse() returns a non-empty string.
+    Verify suggest_solution() returns a non-empty string.
     """
     actual = (
-        pyexcuses.generate_excuse()
-    )  # get the actual return value of generate_excuse()
+        pyexcuses.suggest_solution()
+    )  # get the actual return value of suggest_solution()
     assert isinstance(
         actual, str
-    ), f"Expected generate_excuse() to return a string. Instead, it returned {actual}"
+    ), f"Expected suggest_solution() to return a string. Instead, it returned {actual}"
     assert (
         len(actual) > 0
-    ), f"Expected generate_excuse() not to be empty. Instead, it returned a string with {len(actual)} characters"
+    ), f"Expected suggest_solution() not to be empty. Instead, it returned a string with {len(actual)} characters"
 
 
-def test_generate_excuse_spanish():
+def test_suggest_solution_spanish():
     """
-    Verify generate_excuse() returns a non-empty string in spanish mode.
+    Verify suggest_solution() returns a non-empty string in spanish mode.
     """
-    actual = pyexcuses.generate_excuse(
+    actual = pyexcuses.suggest_solution(
         spoken_language="es"
-    )  # get the actual return value of generate_excuse()
+    )  # get the actual return value of suggest_solution()
     assert isinstance(
         actual, str
-    ), f"Expected generate_excuse() to return a string. Instead, it returned {actual}"
+    ), f"Expected suggest_solution() to return a string. Instead, it returned {actual}"
     assert (
         len(actual) > 0
-    ), f"Expected generate_excuse() not to be empty. Instead, it returned a string with {len(actual)} characters"
+    ), f"Expected suggest_solution() not to be empty. Instead, it returned a string with {len(actual)} characters"
 
 
-def test_generate_excuse_invalid_spokenlanguage():
+def test_suggest_solution_invalid_spokenlanguage():
     """
-    Verify generate_excuse() will fail if given a fake spoken language.
+    Verify suggest_solution() will fail if given a fake spoken language.
     """
     with pytest.raises(SpokenLanguageNotFoundError) as exception:
-        pyexcuses.generate_excuse(spoken_language="no")
+        pyexcuses.suggest_solution(spoken_language="no")
     assert isinstance(
         exception.value, SpokenLanguageNotFoundError
-    ), f"Expected generate_excuse() to return an error. Instead, it returned {type(exception.value)}"
+    ), f"Expected suggest_solution() to return an error. Instead, it returned {type(exception.value)}"
 
 
-def test_generate_excuse_invalid_programminglanguage():
+def test_suggest_solution_invalid_programminglanguage():
     """
-    Verify generate_excuse() will fail if given a fake programming language.
+    Verify suggest_solution() will fail if given a fake programming language.
     """
     with pytest.raises(ProgrammingLanguageNotFoundError) as exception:
-        pyexcuses.generate_excuse(programming_language="c#")
+        pyexcuses.suggest_solution(programming_language="c#")
     assert isinstance(
         exception.value, ProgrammingLanguageNotFoundError
-    ), f"Expected generate_excuse() to return an error. Instead, it returned {type(exception.value)}"
+    ), f"Expected suggest_solution() to return an error. Instead, it returned {type(exception.value)}"
