ccff5a6b6e2b6ffd2f452ebc97c4e43aa58a409f
danny031103 <db4471@nyu.edu>
2024-10-30 16:01:22 -0400

    Added comments

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,7 +1,10 @@
 import pytest
 from funtasks.tasks import add_task, complete_task, random_task, random_daily_goal, tasks
 
+##############################
+#add task tests
 
+#TEST 1
 def test_add_task():
     tasks.clear()
     result=add_task("Do laundry", 3)
@@ -9,3 +12,44 @@ def test_add_task():
     assert tasks["Do laundry"]["urgency"]==3
     assert tasks["Do laundry"]["completed"]==False
 
+#TEST 2
+    
+
+
+#TEST 3
+    
+
+##############################
+#complete task tests
+    
+#TEST 1
+
+
+#TEST 2
+    
+
+#TEST 3
+    
+##############################
+#random task tests
+    
+#TEST 1
+
+
+#TEST 2
+    
+
+#TEST 3
+    
+##############################
+#daily goal task tests
+    
+#TEST 1
+
+
+#TEST 2
+    
+
+#TEST 3
+    
+##############################
