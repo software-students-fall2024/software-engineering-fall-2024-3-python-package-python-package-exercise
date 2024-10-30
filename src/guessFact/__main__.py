4b20ce790172dcae5654fc7c85038bceca7512c3
HannaHan2 <hannahan.021115@gmail.com>
2024-10-29 20:05:30 -0400

    run the unit test for hello_world function

---
+++ src/guessFact/__main__.py
@@ -0,0 +1,7 @@
+import guessFact.code as code
+
+def main() -> None:
+    code.hello_world()
+
+if __name__ == "__main__":
+    main()
---
+++ src/guessFact/code.py
@@ -0,0 +1,5 @@
+"""This file should contains the code that will be implemented"""
+
+def hello_world() -> None:
+    """Prints 'Hello World!' to the console."""
+    print("Hello World!")
