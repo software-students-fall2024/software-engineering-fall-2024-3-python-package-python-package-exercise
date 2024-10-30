719ddcb999992f4b2648024c2e905599cee5fa2c
StephenS2021 <sspencerwong28@gmail.com>
2024-10-30 15:15:13 -0400

    Added new file for logic functions

--- src/pyanimalconverter/__main__.py
+++ src/pyanimalconverter/__main__.py
@@ -23,6 +23,7 @@ animal = '''
 
 def parse_args(args):
     parser = argparse.ArgumentParser()
+    
     parser.add_argument('-fm', '--from-unit', choices=CHOICES, help="Unit to convert from")
     parser.add_argument('-to', '--to-unit', choices=CHOICES, help="Unit to convert to")
     parser.add_argument('--value', type=float, required=True, help="Value of unit")
