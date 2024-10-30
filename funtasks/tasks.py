1fd68e3aea724270f7ec762852db998ad395829b
danny031103 <db4471@nyu.edu>
2024-10-30 15:01:43 -0400

    Added tasks and a new function

--- funtasks/tasks.py
+++ funtasks/tasks.py
@@ -1,6 +1,25 @@
 import random
 
-tasks={}
+tasks={
+    "Finish SWE project": {"urgency": 5, "completed": False},
+    "Take out the trash": {"urgency": 1, "completed": False},
+    "Finish personal project": {"urgency": 4, "completed": False},
+    "Call a friend": {"urgency": 3, "completed": False},
+    "Grocery shopping": {"urgency": 3, "completed": False},
+    "Water the plants": {"urgency": 1, "completed": False},
+    "Go for a run": {"urgency": 3, "completed": False},
+    "Read a book": {"urgency": 2, "completed": False},
+    "Complete a coding challenge": {"urgency": 2, "completed": False},
+    "Plan a weekend getaway": {"urgency": 2, "completed": False},
+    "Clean your room": {"urgency": 5, "completed": False},
+    "Do laundry": {"urgency": 1, "completed": False},
+    "Cook dinner": {"urgency": 3, "completed": False},
+    "Take a nap": {"urgency": 1, "completed": False},
+    "Watch a movie": {"urgency": 2, "completed": False},
+    "Learn a new recipe": {"urgency": 3, "completed": False},
+    "Do a puzzle": {"urgency": 2, "completed": False},
+    "Write a blog post": {"urgency": 4, "completed": False},
+}
 
 #add task
 def add_task(task_name:str,task_urgency:int):
@@ -27,3 +46,18 @@ def random_task():
     else:
         task=random.choice(list(tasks.keys()))
         return f"Random task: {task} with urgency {tasks[task]['urgency']}"
+
+#daily goal
+def random_daily_goal(daily_goal_time:int):
+    sub15tasks=["Take a quick walk","Take a shower","Have a glass of water","Do a handstand","Meditate for 15 minutes","Watch 10 tiktoks"]
+    sub30tasks=["Read a chapter of a book","Run 3 miles","Clean dishes","Write a blog post","Do a 30 minute workout","Watch a show"]
+    longtasks=["Run a marathon","Travel to Australia","Write a book","Adopt a dog","Watch a movie","Learn a new language"]
+    if daily_goal_time<=15:
+        task=random.choice(sub15tasks)
+        return f"Random task: {task}!"
+    elif daily_goal_time>15 and daily_goal_time<=30:
+        task=random.choice(sub30tasks)
+        return f"Random task: {task}!"
+    else:
+        task=random.choice(longtasks)
+        return f"Random task: {task}!"
