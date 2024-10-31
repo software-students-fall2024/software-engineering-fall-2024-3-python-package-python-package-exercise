340adf44529ca5414f325397a0f5d406e02279dc
TamaraBuenoo <109610434+TamaraBuenoo@users.noreply.github.com>
2024-10-31 18:23:33 -0400

    Added ASCII Art and Noise Functions for 'Cow, Cat, Dog, Duck'

--- app.py
+++ app.py
@@ -2,24 +2,75 @@ def parse_input(user_input):
     # Type of the input-make sure string
     # Strip trailing spaces
     while(True):
-      if user_input.strip()!="":
+      if user_input.strip():
         animals = user_input.split(" ")
         #calls print_art or print_noise
 
+        for animal in animals:
+          print_art(animal.strip()) # Calls print_art and, subsequently, print_noise
+        else: 
+          print ("Please enter a valid animal name.")
+    
+
       # getting animal name from the user
-      user_input = input("Enter an animal or exit : ")
+      user_input = input("Enter an animal or type 'exit' : ")
       
-      if user_input == "exit":
+      if user_input.lower() == "exit":
         break
 
-def print_art(animals):
+def print_art(animal):
   #match animal with art
   #calls print_noise or wrong_input
-  arts = {}
+  arts = {
+        "cow": r"""
+         (__)
+         (oo)
+   /------\/
+  / |    ||
+ *  ||---||
+        """,
+        "cat": r"""
+ /\_/\  
+( o.o ) 
+ > ^ <  
+        """,
+        "dog": r"""
+ / \__
+(    @\____
+ /         O
+        """,
+        "duck": r"""
+ __
+<(o )___
+ (  ._> /
+   `---'  
+        """
+    }
+  if animal in arts:
+    print(arts[animal])
+    print_noise(animal)
+  else:
+    wrong_input(animal, length = 1)
 
 def print_noise(animal):
+   """
+    Prints the sound of a given animal after displaying its ASCII art.
+    
+    Parameters:
+    - animal_name (str): The parsed animal name.
+    """
    #match animal with noise
-   noises = {}
+   noises = {
+     "cow": "Moo!",
+     "cat": "Meow!",
+     "dog": "Woof!",
+     "duck": "Quack!"
+     
+     }
+   if animal in noises:
+     print(f"Sound:{noises[animal]}")
+   else:
+     print("Sorry, we don't have a sound for that animal.")
 
 def wrong_input(animals, length):
   #if multiple animals were inputed, print flowers
