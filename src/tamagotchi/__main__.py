18cc3fbc49a6c08ce8db67e9d32752443bdb410a
Jessicakk0711 <kx2053@nyu.edu>
2024-11-01 13:36:11 -0400

    modify main

--- src/tamagotchi/__main__.py
+++ src/tamagotchi/__main__.py
@@ -3,53 +3,64 @@ from PIL import Image
 
 def main():
     print('')
-    print('welcome to tamagotchipy!')
+    print('Welcome to Tamagotchipy!')
     print('')
-    print('here you can:')
+    print('Here you can:')
     print('')
-    print(' ' * 5 + '☆ get your very own tamagotchi')
-    print(' ' * 5 + '☆ or create your very own tamagotchi')
-    print(' ' * 5 + '☆ and name + interact with your tamagotchi')
+    print(' ' * 5 + '☆ Get your very own Tamagotchi')
+    print(' ' * 5 + '☆ Or create your own Tamagotchi')
+    print(' ' * 5 + '☆ And name + interact with your Tamagotchi')
     print('')
 
-    newpet = ''
+    # Create an instance of Tamagotchi
+    tamagotchi_game = t.Tamagotchi()
 
-    pet = int(input('do you want to generate a pet [1] or make your own using an image [2]? please input 1 for option 1 and 2 for option 2: '))
-    
-    while pet != 1 and pet != 2:
-        print('please input 1 or 2 	(╥﹏╥)')
-        pet = int(input('do you want to generate a pet [1] or make your own using an image [2]? please input 1 for option 1 and 2 for option 2: '))
-    
+    # Choose the type of pet
+    try:
+        pet = int(input('Do you want to generate a pet [1] or make your own using an image [2]? Please input 1 for option 1 and 2 for option 2: '))
+        while pet != 1 and pet != 2:
+            print('Please input 1 or 2 	(╥﹏╥)')
+            pet = int(input('Do you want to generate a pet [1] or make your own using an image [2]? Please input 1 for option 1 and 2 for option 2: '))
+    except ValueError:
+        print("Invalid input. Please enter 1 or 2.")
+        return  # Exit if input is not valid
+
     if pet == 1:
-        num = int(input('please input any number from 1 to 11... your new pet is waiting for you!: '))
-        newpet = t.getpet(num)
-        while not(num >= 1 and num <= 11):
-            print('your pet is waiting for you... please put in a valid number! (╥﹏╥): ')
-            num = int(input('please input any number from 1 to 11... your new pet is waiting for you!: '))
-            newpet = t.getpet(num)
-        print(newpet)
+        # Generate a predefined pet
+        try:
+            num = int(input('Please input any number from 1 to 11... Your new pet is waiting for you!: '))
+            while not (1 <= num <= 11):
+                print('Your pet is waiting for you... please put in a valid number! (╥﹏╥)')
+                num = int(input('Please input any number from 1 to 11... Your new pet is waiting for you!: '))
+            tamagotchi_game.ascii_art_label = tamagotchi_game.getpet(num)  # Explicitly set ascii_art_label
+            
+            if not tamagotchi_game.ascii_art_label:
+                print("Error: Predefined pet text could not be loaded.")
+                return  # Exit if no pet content
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 11.")
+            return  # Exit if input is not valid
     else:
-        image_path = str(input('please give a correct file path to your image!: '))
-        print('generating your pet...')
-        print('☆')
-        print('☆')
-        print('☆')
-        print('☆')
-        print('☆')
-        print('')
-        result = t.get_ascii_art(image_path, scale=0.1, character_map=t.G_SCALE_1)
-        while result is None:
-            image_path = str(input('please give a correct file path to your image!: '))
-            print('generating your pet...')
-            print('☆')
-            print('☆')
-            print('☆')
-            print('☆')
-            print('☆')
-            print('')
-            result = t.get_ascii_art(image_path, scale=0.1, character_map=t.G_SCALE_1)
-        newpet = result
+        # Generate ASCII art from an image
+        image_path = str(input('Please provide the file path to your image!: '))
+        print('Generating your pet...')
 
-    print('done')
+        tamagotchi_game.ascii_art_label = tamagotchi_game.get_ascii_art(image_path, scale=0.1, character_map=t.Tamagotchi.G_SCALE_1)
+
+        while tamagotchi_game.ascii_art_label is None:
+            print("Failed to generate ASCII art. Please check the file path and try again.")
+            image_path = str(input('Please provide a correct file path to your image!: '))
+            print('Generating your pet...')
+            tamagotchi_game.ascii_art_label = tamagotchi_game.get_ascii_art(image_path, scale=0.1, character_map=t.Tamagotchi.G_SCALE_1)
+
+    # Display the ASCII art or pet text in the terminal only once
+    print("ASCII Art or Predefined Pet Text:")
+    print(tamagotchi_game.ascii_art_label)  # Print final result
+
+    print('Done!')
+
+    # Start the GUI with the generated pet
+    tamagotchi_game.run_game()
+
 if __name__ == "__main__":
     main()
