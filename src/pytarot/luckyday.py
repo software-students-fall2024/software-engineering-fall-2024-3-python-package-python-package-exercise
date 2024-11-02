ff1364896c7c71ca25170a655f2f618cc964ca94
BomingZhang-coder <bz2196@nyu.edu>
2024-11-02 18:53:09 -0400

    luckyday

--- src/pytarot/luckyday.py
+++ src/pytarot/luckyday.py
@@ -1,11 +1,19 @@
 import random
 
 def get_lucky_day():
-    month = random.randint(1, 12)
 
-    if month in [1, 3, 5, 7, 8, 10, 12]:
+    months = {
+        1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr',
+        5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug',
+        9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'
+    }
+
+    month_num = random.randint(1, 12)
+    month = months[month_num]
+
+    if month_num in [1, 3, 5, 7, 8, 10, 12]:
         day = random.randint(1, 31)
-    elif month in [4, 6, 9, 11]:
+    elif month_num in [4, 6, 9, 11]:
         day = random.randint(1, 30)
     else:  
         day = random.randint(1, 28)
