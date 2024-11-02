26f4f348e5e7149bc38b83346dd7becc180599db
anerivs <aneri.shah@nyu.edu>
2024-11-02 17:20:54 -0400

    installed pytest, fixed bug in test_fortune_cookie.py

--- tests/test_fortune_cookie.py
+++ tests/test_fortune_cookie.py
@@ -2,4 +2,12 @@ from oracle.fortune_cookie import get_fortune, fortunes
     
 #test function
 def test_fortune_cookie():
-    assert get_fortune in fortunes
+    result = get_fortune()
+    assert result in fortunes
+
+def test_fortune_is_string():
+    result = get_fortune()
+    assert isinstance(result, str)
+
+def test_fortunes_not_empty():
+    assert len(fortunes)>0
