2fe005ee3889a5f5df5c1cb253017a54ba34bef8
bryccce <qy765@nyu.edu>
2024-11-03 12:24:22 -0500

    Update Bryccce

---
+++ src/fortunes/get_multiple_fortunes.py
@@ -0,0 +1,25 @@
+import random
+
+def getMultipleFortunes(n):
+    file_path = 'fortune.txt'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        fortunes = file.read().split('%')
+        fortunes = [fortune.strip() for fortune in fortunes if fortune.strip()]
+
+    result = []
+    for _ in range(n):
+        num = random.randint(1, 100)
+        fortune = random.choice(fortunes)
+        while fortune in result:
+            fortune = random.choice(fortunes)
+        result.append([fortune, num])       
+    
+    return result
+
+#Test trial
+# result = getMultipleFortunes(5)
+# for fortune, num in result:
+#     print(f"Fortune: {fortune}\nNumber: {num}\n")
+
+
+
