278e6a1fab9844d2efb9cc60b052a6a58d1ccc6e
safiabillah <safiabillah2004@gmail.com>
2024-11-03 15:08:28 -0500

    function works as expected, now will create test files

---
+++ pyquotes/quotes.py
@@ -0,0 +1,54 @@
+inspirational = [
+    {"quote": "Life is like a box of chocolates. You never know what you're gonna get.", "movie": "Forrest Gump"},
+    {"quote": "Carpe diem. Seize the day, boys. Make your lives extraordinary.", "movie": "Dead Poets Society"},
+    {"quote": "The flower that blooms in adversity is the most rare and beautiful of all.", "movie": "Mulan"},
+    {"quote": "E.T. phone home.", "movie": "E.T. the Extra-Terrestrial"},
+    {"quote": "What we do in life echoes in eternity.", "movie": "Gladiator"},
+    {"quote": "You have to be a bit crazy to do great things.", "movie": "The Pursuit of Happyness"},
+    {"quote": "Just keep swimming.", "movie": "Finding Nemo"},
+    {"quote": "The best way to predict the future is to invent it.", "movie": "The Social Network"},
+    {"quote": "It's what you do right now that makes a difference.", "movie": "Black Hawk Down"},
+    {"quote": "Our lives are defined by opportunities, even the ones we miss.", "movie": "The Curious Case of Benjamin Button"},
+    {"quote": "Dream big and dare to fail.", "movie": "Norma Rae"},
+    {"quote": "Ernest Hemingway once wrote, 'The world is a fine place and worth fighting for.' I agree with the second part.", "movie": "Seven"},
+]
+
+sad = [
+    {"quote": "I'll never let go, Jack. I promise.", "movie": "Titanic"},
+    {"quote": "We all lose our loved ones. It’s part of life.", "movie": "A Walk to Remember"},
+    {"quote": "You can't put your arms around a memory.", "movie": "The Outsiders"},
+    {"quote": "But I knew you’d come back. Because my dad promised me.", "movie": "Interstellar"},
+    {"quote": "Why do fireflies have to die so soon?", "movie": "Grave of the Fireflies"},
+    {"quote": "I'm not afraid of dying. I'm afraid I haven't been alive enough.", "movie": "Meet Joe Black"},
+    {"quote": "I don't regret the things I've done, but those I did not do.", "movie": "Dead Poet's Society"},
+    {"quote": "I don’t want to survive. I want to live!", "movie": "The Pursuit of Happyness"},
+    {"quote": "Thanks for the adventures; now go have a new one! Love, Ellie.", "movie": "Up"},
+]
+
+funny = [
+    {"quote": "I'm not arguing, I'm just explaining why I'm right.", "movie": "The Office (TV Series)"},
+    {"quote": "I’m not bad. I’m just drawn that way.", "movie": "Who Framed Roger Rabbit"},
+    {"quote": "Sticking feathers up your butt does not make you a chicken.", "movie": "Fight Club"},
+    {"quote": "She doesn't even go here!", "movie": "Mean Girls"},
+    {"quote": "Hold my Poodle!", "movie": "White Chicks"},
+    {"quote": "She's a full-on Monet. It's like a painting, see? From far away, it's okay. But up close, it's a big old mess", "movie": "Clueless"},
+    {"quote": "Why can't you just be happy for me and then go home and talk behind my back later like a normal person!?", "movie": "Bridesmaids"},
+    {"quote": "Say hello to my little friend", "movie": "Scarface"},
+]
+
+iconic = [
+    {"quote": "Why so serious?", "movie": "The Dark Knight"},
+    {"quote": "To be or not to be, that is the question.", "movie": "Hamlet"},
+    {"quote": "May the Force be with you.", "movie": "Star Wars"},
+    {"quote": "There's no place like home.", "movie": "The Wizard of Oz"},
+    {"quote": "I'm king of the world!", "movie": "Titanic"},
+    {"quote": "I simply am not there.", "movie": "American Psycho"},
+    {"quote": "For Narnia and for Aslan!", "movie": "Narnia"},
+    {"quote": "To infinity and beyond!", "movie": "Toy Story"},
+]
+quote_categories = {
+    "inspirational": inspirational,
+    "sad": sad,
+    "funny": funny,
+    "iconic": iconic
+}
