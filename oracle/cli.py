ee696f474a96f9fbaf4ff4e9927392b9ac367318
anerivs <aneri.shah@nyu.edu>
2024-11-03 17:07:39 -0500

    added arguments to fortune cookie

--- oracle/cli.py
+++ oracle/cli.py
@@ -5,21 +5,26 @@ from fortune_cookie import get_fortune
 
 def error_msg():
     print("To contact the oracle, type:")
-    print("fortune_cookie to receive your fortune")
+    print("fortune_cookie <mood> to receive your fortune (e.g. optimistic, realistic, unfortunate)")
     print("eight_ball to make a decision")
     print("petting_zoo to feed your pet")
     print("vibe_check to see what's up")
 
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) == 1:
         error_msg()
         return
     
     command = sys.argv[1].lower()
 
     if command == "fortune_cookie":
-        print(get_fortune())
+        if len(sys.argv) == 3:
+            mood = sys.argv[2].lower()
+            print(get_fortune(mood))
+        else:
+            print("please specify a mood: optimistic, realistic, unfortunate")
+            error_msg()
     elif command == "eight_ball":
         print("not implemented yet")
     elif command == "petting_zoo":
