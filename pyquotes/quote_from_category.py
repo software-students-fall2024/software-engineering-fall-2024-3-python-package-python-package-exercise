763cada0fc8f592b87029eb0c70d44e02e68bd3e
safiabillah <safiabillah2004@gmail.com>
2024-11-03 16:29:19 -0500

    added test functionality

--- pyquotes/quote_from_category.py
+++ pyquotes/quote_from_category.py
@@ -6,9 +6,10 @@ def list_quote_from_category(movie_name):
             if quote["movie"].lower() == movie_name.lower():
                 results.append(f'"{quote["quote"]}" - {category.capitalize()}')
     if results:
-        print(f'Quotes from "{movie_name}":')
-        for quote in results:
-            print(quote)
+        # print(f'Quotes from "{movie_name}":')
+        # for quote in results:
+        #     print(quote)
+        return results
     else:
         print('There are currently no quotes from this movie, sorry!')
 
