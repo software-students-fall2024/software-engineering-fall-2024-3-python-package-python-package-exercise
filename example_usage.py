4730ae9bc638b93df638d76f4c06b5db343af56d
jh7316 <jh7316@nyu.edu>
2024-11-04 04:09:57 -0500

    update example usage

--- example_usage.py
+++ example_usage.py
@@ -1,11 +1,12 @@
 from Financiers.stock import Stock
+import pandas as pd
 
 stock = Stock()
 
 print(stock.get_market_mood())
 print (stock.get_earnings("AAPL"))
 print(stock.get_price_data("AAPL"))
-print(stock.forecast_prices("AAPL"))
+print(stock.forecast_prices("AAPL")) 
 print(stock.company_overview("IBM"))
 print(stock.plot_top_movers(["AAPL", "GOOGL","IBM"]))
 print(stock.project_future_estimates(["AAPL", "GOOGL","IBM"]))
@@ -15,5 +16,5 @@ data = {
     'low': [8, 8, 9, 10, 10, 10, 10, 10, 10, 10],
     'close': [9, 9, 10, 11, 11, 11, 11, 11, 11, 11]
 }
-print(stock.calculate_atr(data))
+print(stock.calculate_atr(pd.DataFrame(data)))
 
