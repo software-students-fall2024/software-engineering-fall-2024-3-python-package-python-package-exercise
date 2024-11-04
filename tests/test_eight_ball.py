5b776a40a1b1a1c8760705993c5da323b0601fa0
cl6591@nyu.edu <cl6591@nyu.edu>
2024-11-03 23:42:16 -0500

    fixed test cases

--- tests/test_eight_ball.py
+++ tests/test_eight_ball.py
@@ -2,17 +2,17 @@ from oracle.eight_ball import get_eight_ball, responses
     
 #test function
 def test_eight_ball():
-    result = get_eight_ball()
+    result = get_eight_ball(1)
     assert result in responses
 
 def test_response_is_string():
-    result = get_eight_ball()
+    result = get_eight_ball(1)
     assert isinstance(result, str)
 
 def test_response_not_empty():
-    result = get_eight_ball()
+    result = get_eight_ball(1)
     assert result != ""
 
 def test_random():
-    results = {get_eight_ball() for _ in range(100)}
+    results = {get_eight_ball(1) for _ in range(100)}
     assert len(results) > 1
